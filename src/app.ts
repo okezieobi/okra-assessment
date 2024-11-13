580668db951ccf20880e2b8b2e35b5f73b9f50bb
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2024-11-13 10:45:35 +0000

    api update

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import logger from "morgan";
 import usersRouter from "./routes/users";
 
 const app = express();
+const router = express.Router();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/users", usersRouter);
+router.use("/users", usersRouter);
+app.use("/api/v1", router);
 
 app.listen();
 export default app;
