3e53e3d5749b7b59f50800291864dcb7c9794519
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2024-11-13 09:29:23 +0000

    testing prettier

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import logger from 'morgan';
+import express from "express";
+import logger from "morgan";
 
-import usersRouter from './routes/users';
+import usersRouter from "./routes/users";
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
-app.listen()
+app.listen();
 export default app;
