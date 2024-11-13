d20f81ad4ab95e02ae10d7feb0abbb931c5c6bbf
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2024-11-13 11:26:58 +0000

    saving progress

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", async function (req, res) {
-  const users = await UserCollection.findOne();
+  const users = await UserCollection.find({}).toArray();
   res.send(users);
 });
 
