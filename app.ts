e0a95040a96709bd051a19e50882de32fe25ec42
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2024-11-13 09:26:48 +0000

    save progress

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import logger from 'morgan';
+
+import usersRouter from './routes/users';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRouter);
+
+app.listen()
+export default app;
